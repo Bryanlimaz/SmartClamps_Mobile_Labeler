c0494f4ab94199ef360f516922dd94119ef0d4d0
Bryan Lima <Bryanlimaz@users.noreply.github.com>
2024-08-22 16:48:26 -0300

    Test on app.js

--- App.js
+++ App.js
@@ -3,18 +3,44 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.mainContainer}>
+      <View style={styles.subContainer1}>
+        <Text>Hello World!!</Text>
+      </View>
+      <View style={styles.subContainer2}>
+      <Text>Hello World 2!!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  mainContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1, // Expressa a ocupação da tela inteira.
+    // height: 850,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  subContainer1: {
+    display: 'flex',
+    flex: 1/4,
+    // width: 100,
+    // height: 100,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subContainer2: {
+    display: 'flex',
+    flex: 1/4,
+    // width: 100,
+    // height: 100,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
